a156c91568009f4da69eb84bd3356643daacd9d0
arjanbe <arjan@bekkema.net>
2023-08-10 23:39:48 +0200

    Added tags as posible foldername

--- Radarr_move_movies.ps1
+++ Radarr_move_movies.ps1
@@ -4,6 +4,9 @@ $MovieRootFolder = "N:\grpdata\Video\__Nieuw"
 
 $Counter = 0
 
+# Fetch all tags
+$Tags = Invoke-RestMethod -Uri "$RadarrURL/api/v3/tag?apikey=$APIkey" 
+
 # Fetch all movies from Radarr
 $Movies = Invoke-RestMethod -Uri "$RadarrURL/api/v3/movie?apikey=$APIkey" 
 
@@ -14,20 +17,32 @@ ForEach ($Movie in $Movies)
     # ForEach movie check if it has a movie file
     If ($Movie.hasFile)
     {
+        # Check if there is a tag
+        If (($Movie.tags).Count -gt 0)
+        {
+            $LabelTemp = ($Tags | where {$_.id -eq $Movie.tags[0]}).label
+            $Label = $LabelTemp.Substring(0,1).ToUpper() + $LabelTemp.Substring(1)
+            $NewMoviePath = "$MovieRootFolder\_$Label"
+        }
+        Else
+        {
+            $NewMoviePath = "$MovieRootFolder\_$($Movie.genres[0])"
+        }
+
         # If there is a movie file, is it already in the correct folder. genres[0] is the first genre in the list.
-        If ($Movie.path -like "$MovieRootFolder\_$($Movie.genres[0])*")
+        If ($Movie.path -like "$NewMoviePath\*")
         {
             # movie is alreay in the right folder. nothing todo
-            Write-Host "Processing: $($Movie.Title) with movieid $($Movie.id) is in _$($Movie.genres[0])" -ForegroundColor Green
+            # Write-Host "Processing: $($Movie.Title) with movieid $($Movie.id) is in _$($Movie.genres[0])" -ForegroundColor Green
         }
         Else
         {
             # movie needs to be moved to the new location using the API
-            Write-Host "Processing: $($Movie.Title) with movieid $($Movie.id) and moving it to _$($Movie.genres[0])" -ForegroundColor Yellow
+            Write-Host "Processing: $($Movie.Title) with movieid $($Movie.id) and moving it to $NewMoviePath" -ForegroundColor Yellow
             $Data = @{
                 movieIds = @($Movie.id)
                 moveFiles = $True
-                rootFolderPath = "$MovieRootFolder\_$($Movie.genres[0])"
+                rootFolderPath = $NewMoviePath
             }
             $json = $data | ConvertTo-Json;
             $Result = Invoke-RestMethod -Method PUT -Uri "$RadarrURL/api/v3/movie/editor?apikey=$APIkey" -ContentType "application/json" -Body $json
